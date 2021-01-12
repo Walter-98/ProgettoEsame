09e6b263f7135f0d4ae43238b63345070ca30ad5
Walter-98 <75211109+Walter-98@users.noreply.github.com>
2021-01-12 17:48:14 +0100

    Delete src directory

--- src/main/java/com/esame/Nuvolosita/Filter/Filter.java
+++
@@ -1,9 +0,0 @@
-package com.esame.Nuvolosita.Filter;
-import com.esame.Nuvolosita.Service.*;
-public interface Filter {
-	public abstract  void FilterMain();
-	public abstract  void FilterTimezone();
-	public abstract  void FilterClouds();
-	public abstract  void FilterWind();
-	public abstract  void FilterWeather();
-}
